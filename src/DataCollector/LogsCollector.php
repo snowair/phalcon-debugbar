54666afd716112010ce11047f9e7caad752f0d12
snowair <xcoolcc@gmail.com>
2015-03-12 23:04:37 +0800

    add LogsCollector and Logger\Adapter\Debugbar

--- src/DataCollector/LogsCollector.php
+++ src/DataCollector/LogsCollector.php
@@ -8,6 +8,61 @@
 namespace Snowair\Debugbar\DataCollector;
 
 
-class LogsCollector {
+use DebugBar\DataCollector\DataCollector;
+use Phalcon\DI;
+use Phalcon\Logger\Adapter;
+use Phalcon\Logger\Multiple;
+use Snowair\Debugbar\Phalcon\Logger\Adapter\Debugbar;
 
+class LogsCollector extends DataCollector{
+
+	protected $_logs = array();
+
+	public function __construct( DI $di ) {
+		if ( $di->has('log') && $log = $di['log'] ) {
+			$debugbar_loger = new Debugbar($di['debugbar']);
+			if ( $log instanceof Adapter ) {
+				$multiple = new Multiple();
+				$multiple->push( clone $log );
+				$multiple->push( $debugbar_loger );
+				/** @var DI\Service $service */
+				$service = $di->getService('log');
+				$service->setSharedInstance($multiple);
+				$service->setDefinition($multiple);
+			}elseif($log instanceof Multiple){
+				$log->push( $debugbar_loger );
+			}
+		}
+	}
+
+	public function log( $message, $type, $time, $context ) {
+		$this->_logs[]=array(
+			'message'=>$message,
+			'type'=>$type,
+			'time'=>$time,
+			'context'=>$context,
+		);
+	}
+	/**
+	 * Called by the DebugBar when data needs to be collected
+	 * @return array Collected data
+	 */
+	function collect() {
+		$logs = array();
+		foreach ( $this->_logs as $log ) {
+			$this->getDataFormatter()->formatVar($log);
+		}
+		return array(
+			'logs'=>$logs,
+			'count'=>count($logs),
+		);
+	}
+
+	/**
+	 * Returns the unique name of the collector
+	 * @return string
+	 */
+	function getName() {
+		return 'log';
+	}
 }
--- src/Phalcon/Logger/Adapter/Debugbar.php
+++ src/Phalcon/Logger/Adapter/Debugbar.php
@@ -7,7 +7,43 @@
 
 namespace Snowair\Debugbar\Phalcon\Logger\Adapter;
 
+use Phalcon\Logger\Adapter;
+use Phalcon\Logger\Formatter\Line;
+use Snowair\Debugbar\PhalconDebugbar;
 
-class Debugbar {
+class Debugbar extends Adapter{
 
+	/**
+	 * @var PhalconDebugbar $_debugbar
+	 */
+	protected $_debugbar;
+
+	public function __construct( PhalconDebugbar $debugbar ) {
+		$this->_debugbar = $debugbar;
+	}
+
+	protected function logInternal( $message, $type, $time, $context ) {
+		if ($this->_debugbar->hasCollector('log') && $this->_debugbar->shouldCollect('log') ) {
+			$this->_debugbar->getCollector('log')->log($message,$type,$time,$context);
+		}
+	}
+
+	/**
+	 * Returns the internal formatter
+	 * @return \Phalcon\Logger\FormatterInterface
+	 */
+	public function getFormatter() {
+		if ( !is_object($this->_formatter) ){
+			$this->_formatter = new Line();
+		}
+		return $this->_formatter;
+	}
+
+	/**
+	 * Closes the logger
+	 * @return boolean
+	 */
+	public function close() {
+		return true;
+	}
 }
