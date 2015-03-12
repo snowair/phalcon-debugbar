f53fd44afb6f4cb6ac83721041809b96d1f9e7bb
snowair <xcoolcc@gmail.com>
2015-03-12 23:04:22 +0800

    add LogsCollector and Logger\Adapter\Debugbar

---
+++ src/DataCollector/LogsCollector.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: zhuyajie
+ * Date: 15/3/12
+ * Time: 22:27
+ */
+
+namespace Snowair\Debugbar\DataCollector;
+
+
+class LogsCollector {
+
+}
---
+++ src/Phalcon/Logger/Adapter/Debugbar.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: zhuyajie
+ * Date: 15/3/12
+ * Time: 22:03
+ */
+
+namespace Snowair\Debugbar\Phalcon\Logger\Adapter;
+
+
+class Debugbar {
+
+}
