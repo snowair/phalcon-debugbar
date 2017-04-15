2aca6815e46bf105702bd53e86e74a9f2c2feaa1
snowair <xcoolcc@gmail.com>
2017-04-15 14:58:50 +0800

    upgrade to Whoops2

--- src/Whoops/WhoopsServiceProvider.php
+++ src/Whoops/WhoopsServiceProvider.php
@@ -6,11 +6,16 @@
  */
 
 namespace Snowair\Debugbar\Whoops;
-
+/**
+ * Whoops - php errors for cool kids
+ * @author Filipe Dobreira <http://github.com/filp>
+ */
 use Phalcon\DI;
+use Phalcon\DI\Exception;
+use Phalcon\Dispatcher;
 use Whoops\Handler\JsonResponseHandler;
+use Whoops\Handler\PrettyPageHandler;
 use Whoops\Run;
-
 class WhoopsServiceProvider
 {
     /**
@@ -25,31 +30,60 @@ class WhoopsServiceProvider
         if (!$di) {
             $di = DI::getDefault();
         }
+        /** @var Dispatcher $dispatcher */
+        $dispatcher =$di->getShared('dispatcher');
+        $eventManager = $dispatcher->getEventsManager();
+        $eventManager->detachAll('dispatch:beforeException');
 
         // There's only ever going to be one error page...right?
         $di->setShared('whoops.pretty_page_handler', function () use($di) {
             return (new DebugbarHandler())->setDi($di);
         });
-
         // There's only ever going to be one error page...right?
         $di->setShared('whoops.json_response_handler', function () {
             $jsonHandler = new JsonResponseHandler();
-            $jsonHandler->onlyForAjaxRequests(true);
             return $jsonHandler;
         });
-
-
-        $di->setShared('whoops', function () use ($di) {
+        // Retrieves info on the Phalcon environment and ships it off
+        // to the PrettyPageHandler's data tables:
+        // This works by adding a new handler to the stack that runs
+        // before the error page, retrieving the shared page handler
+        // instance, and working with it to add new data tables
+        $phalcon_info_handler = function () use ($di) {
+            try {
+                $request = $di['request'];
+            } catch (Exception $e) {
+                // This error occurred too early in the application's life
+                // and the request instance is not yet available.
+                return;
+            }
+            // Request info:
+            $di['whoops.pretty_page_handler']->addDataTable('Phalcon Application (Request)', array(
+                'URI'         => $request->getScheme().'://'.$request->getServer('HTTP_HOST').$request->getServer('REQUEST_URI'),
+                'Request URI' => $request->getServer('REQUEST_URI'),
+                'Path Info'   => $request->getServer('PATH_INFO'),
+                'Query String' => $request->getServer('QUERY_STRING') ?: '<none>',
+                'HTTP Method' => $request->getMethod(),
+                'Script Name' => $request->getServer('SCRIPT_NAME'),
+                //'Base Path'   => $request->getBasePath(),
+                //'Base URL'    => $request->getBaseUrl(),
+                'Scheme'      => $request->getScheme(),
+                'Port'        => $request->getServer('SERVER_PORT'),
+                'Host'        => $request->getServerName(),
+            ));
+        };
+        $di->setShared('whoops', function () use ($di,$phalcon_info_handler) {
             $run = new Run();
             $run->silenceErrorsInPaths(array(
                 '/phalcon-debugbar/'
             ),E_ALL);
             $run->pushHandler($di['whoops.pretty_page_handler']);
-            $run->pushHandler($di['whoops.json_response_handler']);
+            $run->pushHandler($phalcon_info_handler);
+            if (\Whoops\Util\Misc::isAjaxRequest()){
+                $run->pushHandler($di['whoops.json_response_handler']);
+            }
             return $run;
         });
-
         $di['whoops']->register();
     }
-
 }
